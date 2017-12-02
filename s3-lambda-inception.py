5eb9bb277787830c4006605d960075fd37aa329e
Ryan J. Geyer <me@ryangeyer.com>
2017-12-01 18:54:19 -0800

    I figured out how to use invoke parameters rather than environment vars :facepalm:

--- s3-lambda-inception.py
+++ s3-lambda-inception.py
@@ -3,6 +3,6 @@ import boto3
 import urllib.request
 
 def handler(event, context):
-    urllib.request.urlretrieve(os.environ['URI'], "/tmp/tmp.zip")
+    urllib.request.urlretrieve(event['URI'], "/tmp/tmp.zip")
     conn = boto3.client('s3')
-    conn.upload_file("/tmp/tmp.zip", os.environ['S3_BUCKET'], os.environ['S3_FILENAME'])
+    conn.upload_file("/tmp/tmp.zip", event['S3_BUCKET'], event['S3_FILENAME'])
