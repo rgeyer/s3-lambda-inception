93417db7f7c4daa8382d34feb23c1423895a40a7
Ryan J. Geyer <me@ryangeyer.com>
2017-12-08 16:52:31 -0800

    Travis deploy fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 zip -r s3-lambda-inception.zip s3-lambda-inception.py
-base64_zip_string=$(base64 s3-lambda-inception.zip)
-sed "s,@@BASE64_ZIP_STRING,\"$base64_zip_string\"," package.cat.rb.template > package.cat.rb
+base64_zip_string=$(base64 -w0 s3-lambda-inception.zip)
+sed_cmd="s,@@BASE64_ZIP_STRING,$base64_zip_string,"
+sed $sed_cmd package.cat.rb.template > package.cat.rb
