48208a3f966e5ad3aed5708398aad3312fdb4374
Ryan J. Geyer <me@ryangeyer.com>
2017-12-08 16:32:37 -0800

    POSIX standard sed, rather than OSX specific stuff

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 zip -r s3-lambda-inception.zip s3-lambda-inception.py
-cp package.cat.rb.template package.cat.rb
 base64_zip_string=$(base64 s3-lambda-inception.zip)
-sed -i '' "s,@@BASE64_ZIP_STRING,\"$base64_zip_string\"," package.cat.rb
+sed "s,@@BASE64_ZIP_STRING,\"$base64_zip_string\"," package.cat.rb.template > package.cat.rb
